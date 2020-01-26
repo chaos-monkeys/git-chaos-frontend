10023e8023988c523e02f3982707be3ea2a67d78
Ollie Williams <ollie.williams@ibm.com>
2020-01-26 22:50:23 +0000

    updated path

--- gatsby-node.js
+++ gatsby-node.js
@@ -60,7 +60,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       // (or `node.frontmatter.slug`)
       path: node.fields.slug,
       // This component will wrap our MDX content
-      component: path.resolve("./src/components/page-layout.tsx"),
+      component: path.resolve("./src/templates/post/post.tsx"),
       // You can use the values in this context in
       // our page layout component
       context: { id: node.id },
