e83977b75dbf866d677ca2c18d807eb95bc75b48
robalaban <rob.balaban@gmail.com>
2020-01-27 20:27:21 +0000

    Fixes hamburger banana on iOS

--- src/components/hamburger/hamburger.tsx
+++ src/components/hamburger/hamburger.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import * as styles from './hamburger.module.scss';
-import { useAppState, useAppDispatch } from '../../context/appContext'
-import toggleSidebar from '../../utils/toggleSidebar'
+import React from "react";
+import * as styles from "./hamburger.module.scss";
+import { useAppState, useAppDispatch } from "../../context/appContext";
+import toggleSidebar from "../../utils/toggleSidebar";
 
 // NOTE: SOMEHOW require stops a path error with typescript and svg (and all non-code assets?)
-const OpenIcon = require('../../images/icons/open.inline.svg');
-const CloseIcon = require('../../images/icons/closed.inline.svg');
+const OpenIcon = require("../../images/icons/open.inline.svg");
+const CloseIcon = require("../../images/icons/closed.inline.svg");
 
-
-const hamburgerIcon = (open: boolean) => (open ? (
-  <OpenIcon className={styles.icon} />
-) : (
-  <CloseIcon className={styles.icon} />
-  ));
-
+const hamburgerIcon = (open: boolean) =>
+  open ? (
+    <OpenIcon className={styles.icon} />
+  ) : (
+    <CloseIcon className={styles.icon} />
+  );
 
 const Hamburger = ({ isAnimating }: { isAnimating: boolean }) => {
   const dispatch = useAppDispatch();
-  const { sidebar } = useAppState()
+  const { sidebar } = useAppState();
 
   return (
     <button
@@ -26,15 +25,17 @@ const Hamburger = ({ isAnimating }: { isAnimating: boolean }) => {
       data-open={sidebar.open}
       aria-expanded={sidebar.open}
       className={styles.hamburger}
-      onClick={() => toggleSidebar({
-        isAnimating,
-        isOpen: sidebar.open,
-        dispatch
-      })}
+      onClick={() =>
+        toggleSidebar({
+          isAnimating,
+          isOpen: sidebar.open,
+          dispatch,
+        })
+      }
     >
       {hamburgerIcon(sidebar.open)}
     </button>
-  )
+  );
 };
 
 export default Hamburger;
