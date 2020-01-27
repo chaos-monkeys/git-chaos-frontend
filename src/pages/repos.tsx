db05b8a35316466b41f04cdbd1662a074a2b3e83
Ollie Williams <ollie.williams@ibm.com>
2020-01-27 21:17:26 +0000

    added capability to show multiple repos from many datasources

--- src/pages/repos.tsx
+++ src/pages/repos.tsx
@@ -1,34 +1,73 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-interface ReposIndex {
-  "data": {
-    "allReposJson": {
-      "edges": [
-        {
-          "node": {
-            "id": string,
-            "meta": {
-              "repo_name": string,
-              "repo_owner": string,
-              "start_time": number,
-              "commit_sha": string,
-            }
-          }
-        }
-      ]
-    }
-  }
+ interface Meta {
+  "repo_name": string;
+  "repo_owner": string;
+  "start_time": number;
+  "commit_sha": string;
+  "issue_number": string;
+  "issue_number_title": string;
 }
 
-const ReposIndex = ({ data }: ReposIndex) => {
+ interface Node {
+  "id": string;
+  "meta": Meta;
+}
+
+ interface Edge {
+  "node": Node;
+}
+
+ interface AllReposJson {
+  "edges": Edge[];
+}
+
+ interface Data {
+  "allReposJson": AllReposJson;
+}
+
+ interface RootObject {
+  "data": Data;
+}
+
+
+const buildRepo = (node: Node) => {
+  const {
+    repo_name: repoName,
+    repo_owner: repoOwner,
+    issue_number: issueNumber,
+    issue_number_title: issueNumberTitle,
+  } = node.meta;
+
+  const slug = ["git-chaos",
+    repoOwner,
+    repoName,
+    issueNumber
+  ].join("/");
+
+  return (
+    <li key={node.id}>
+      <Link to={slug}>
+        <h1>{repoName}</h1>
+        <h2>{repoOwner}</h2>
+        {issueNumberTitle}
+      </Link>
+    </li>
+  )
+}
+
+const buildRepoList = (edges: Edge[]) => edges.map(({ node } : Edge) => buildRepo(node))
+
+const ReposIndex = ({ data }: RootObject) => {
   const { edges } = data.allReposJson
-  const { repo_name: repoName } = edges[0].node.meta
-  const { repo_owner: repoOwner } = edges[0].node.meta
+  const repoList = buildRepoList(edges)
+
   return (
     <div>
-      <h1>{repoName}</h1>
-      <h2>{repoOwner}</h2>
+      <ul>
+        {repoList}
+      </ul>
     </div>
   );
 };
@@ -42,8 +81,6 @@ query {
           meta {
             repo_name
             repo_owner
-            start_time
-            commit_sha
             issue_number
             issue_number_title
           }
